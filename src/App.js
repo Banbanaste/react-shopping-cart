aae6ec3625fe2e36b910c899b18829c1cc0c8608
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-01-20 17:05:37 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from "./components/ShoppingCart";
 
 // contexts
 import { ProductContext } from "./contexts/createContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -21,16 +22,17 @@ function App() {
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={cart}>
+          <Navigation />
 
-      {/* Routes */}
-      <Route
-        exact
-        path="/"
-        render={() => <Products products={products} addItem={addItem} />}
-      />
+          {/* Routes */}
 
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route exact path="/" component={Products} />
+
+          <Route path="/cart" render={() => <ShoppingCart />} />
+        </CartContext.Provider>
+      </ProductContext.Provider>
     </div>
   );
 }
